a7e341af74d2e783b7dd7d358d818e7d163cf5d5
kkobayashiHMC <kohei.kobayashi@hmcom.co.jp>
2026-01-16 19:49:09 +0900

    urls.py修正

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("worldmap.urls")),
-    path("django_plotly_dash/", include("django_plotly_dash.urls")),
     path("admin/", admin.site.urls),
 ]

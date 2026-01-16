3e2e06e1d8a3df0e0f6d41b44c9437e5c4a4c66c
kkobayashiHMC <kohei.kobayashi@hmcom.co.jp>
2026-01-16 17:13:40 +0900

    urls.py修正

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from django.http import HttpResponse
 
 urlpatterns = [
-    path("", lambda request: HttpResponse("It works")),
+    path("", TemplateView.as_view(template_name="worldmap/templates/worldmap/index.html")),
     path("admin/", admin.site.urls),
 ]
